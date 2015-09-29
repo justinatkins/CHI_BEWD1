55cd9a2fa5a668a433537d645ef8216443e18f36
Bill Patrianakos <bill@linus>
2015-09-28 21:34:21 -0500

    Add code samples and exercises for week 2 lesson 1.

---
+++ Week2/Lesson1/instructor_notes/arrays.rb
@@ -0,0 +1,12 @@
+# Create a new array and assign it to a variable
+# Check its class
+# !! A class tells you what things this object can respond to !!
+# Create a new array
+# Get size with count, size, length, empty?
+# Read from an array, zero indexing
+# Append to the array, with push, << and unshift
+# Show popping and shifting
+# Show how include? works
+# Show BANG Methods #sort!, #uniq!, #shuffle!, reverse! 
+# Show join, to_s
+# Show split from string

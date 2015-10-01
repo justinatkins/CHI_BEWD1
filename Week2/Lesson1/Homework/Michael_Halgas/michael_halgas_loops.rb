2bf6ac6efc44e3d9eaee9d499556bb805c19d677
mhalgas343 <m.e.halgas@gmail.com>
2015-10-01 12:42:30 -0500

    Homework: Week 2 Lesson 1

--- Week2/Lesson1/Homework/Michael_Halgas/michael_halgas_loops.rb
+++ Week2/Lesson1/Homework/Michael_Halgas/michael_halgas_loops.rb
@@ -11,19 +11,22 @@
 # You take one down and pass it around,
 # No more bottles of beer on the wall :-(
 
-99.downto(1) do |bot|
-	if bot > 2
-	puts "#{bot} bottles of beer on the wall
-	#{bot} bottles of beer!
-	you take one down and pass it around
-	#{bot -= 1} bottles of beer on the wall"
-	else 
-		"#{bot} bottle of beer on the wall
-		#{bot} bottle of beer
-		you take one down and pass it around
-		no more bottles of beer on the wall :-("
-	end
-end
+#Either works --
+#99.downto(1) do |bot|
+#	if bot > 1
+#	puts "#{bot} bottles of beer on the wall
+#	#{bot} bottles of beer!
+#	you take one down and pass it around
+#	#{bot -= 1} bottles of beer on the wall"
+#	else bot == 1
+#	puts "#{bot} bottle of beer on the wall
+	#{bot} bottle of beer
+#	you take one down and pass it around
+#	no more bottles of beer on the wall :-("
+#	end
+#end
+
+bot = 99
 
 while bot > 0
 	if bot > 1
@@ -32,7 +35,7 @@ while bot > 0
 		you take one down and pass it around
 		#{bot -= 1} bottles of beer on the wall"
 	else bot == 1
-		"1 bottle of beer on the wall
+		puts "1 bottle of beer on the wall
 		1 bottle of beer!
 		you take one down and pass it around,
 		no more bottles of beer on the wall :-("
